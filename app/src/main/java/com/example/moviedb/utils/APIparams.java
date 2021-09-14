882f920ac85cc350f9d989c2d6174e1cd62837ef
evaHallermeier <70845526+evaHallermeier@users.noreply.github.com>
2021-09-15 01:12:08 +0300

    initial commit

---
+++ app/src/main/java/com/example/moviedb/utils/APIparams.java
@@ -0,0 +1,8 @@
+package com.example.moviedb.utils;
+
+public class APIparams { //parameters of the API created in TMDB
+
+    public static final String basicURL = "https://api.themoviedb.org";
+    public static final String API_key = "08e696dd9eee56bfad34f61503057b69";
+
+}
